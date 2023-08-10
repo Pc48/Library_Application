6af3b5ce2f2f3847d33d40507fae3bf1a0f18b72
Pc48 <133551940+Pc48@users.noreply.github.com>
2023-08-10 09:38:38 -0400

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,17 +1,4 @@
 
-/*//////////////////////////////////////////////////////////////////////////
-                            MS- 5
-Full Name  :   Preet Chakrani
-Student ID#:   113115224
-Email      :   pchakrani@myseneca.ca
-Section    :   NGG
-
-Authenticity Declaration:
-
-I declare ubmission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. sbmitted
-piece of work is entirely of my own creation.
-//////////////////////////////////////////////////////////////////////////*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
