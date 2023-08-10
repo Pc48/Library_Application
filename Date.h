221cf0f5d73d022cf8a902c708f5bda7e9ed4726
Pc48 <133551940+Pc48@users.noreply.github.com>
2023-08-10 09:37:42 -0400

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,16 +1,4 @@
-/*//////////////////////////////////////////////////////////////////////////
-                            MS- 5
-Full Name  :   Preet Chakrani
-Student ID#:   113115224
-Email      :   pchakrani@myseneca.ca
-Section    :   NGG
 
-Authenticity Declaration:
-
-I declare ubmission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. sbmitted
-piece of work is entirely of my own creation.
-//////////////////////////////////////////////////////////////////////////*/
 
 #ifndef SDDS_DATE_H__
 #define SDDS_DATE_H__
