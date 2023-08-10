d30c8a7a2b8d8d2843dd2dd1d3b5f368700cb097
Pc48 <133551940+Pc48@users.noreply.github.com>
2023-08-10 09:39:43 -0400

    Update Streamable.cpp

--- Streamable.cpp
+++ Streamable.cpp
@@ -1,16 +1,4 @@
-/*//////////////////////////////////////////////////////////////////////////
-                            MS- 5
-Full Name  :   Preet Chakrani
-Student ID#:   113115224
-Email      :   pchakrani@myseneca.ca
-Section    :   NGG
 
-Authenticity Declaration:
-
-I declare ubmission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. sbmitted
-piece of work is entirely of my own creation.
-//////////////////////////////////////////////////////////////////////////*/
 
 #include "Streamable.h"
 using namespace std;
